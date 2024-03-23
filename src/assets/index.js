999a29afacddffd276e3b9e57c664004cd16e50a
Daniel <floricela999@gmail.com>
2024-03-23 21:21:02 +0100

    finshed compress of assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -13,7 +13,6 @@ import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
@@ -44,7 +43,6 @@ export {
   git,
   html,
   javascript,
-  mongodb,
   nodejs,
   reactjs,
   redux,
